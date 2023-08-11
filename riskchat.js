12cb8a1621ef78eb015e3e394563d4c253041b98
bp117 <bindupriya117@gmail.com>
2023-08-12 00:13:12 +0530

    Update riskchat.js

--- riskchat.js
+++ riskchat.js
@@ -1,49 +1,87 @@
-const [chat, setChat] = useState([]);
-const [chat, setChat] = useState([]);
-const [responseData, setResponseData] = useState(null);
-if (response.ok) {
-    const data = await response.json();
+import React, { useState, useRef, useEffect } from 'react';
+import { Tab, Tabs, Card, CardHeader, CardContent, Typography, Link } from '@mui/material';
+import bot from './chatgpt.png';
+import user from './me.png';
+import './App.css';
 
-    // Update responseData
-    setResponseData(data.response.result); 
+function RiskChatExplorer({ chatInputText, setChatInputText }) {
+    const [chat, setChat] = useState([]);
+    const [tabValue, setTabValue] = useState(0);  // State to manage currently active tab
+    // ... [rest of the useState and other functions]
 
-    const botMessage = data.bot && data.bot.trim();
-    if (botMessage) {
-        setChat(prevChat => {
-            const updatedChat = [...prevChat];
-            updatedChat[updatedChat.length - 1].value = botMessage;
-            return updatedChat;
-        });
-    } else {
-        setChat(prevChat => prevChat.slice(0, -1));  // Remove the placeholder
-    }
-}
-<div id="chat_container" ref={chatContainer}>
-    {chat.map((item, index) => (
-        // ... (existing chat messages rendering logic)
-    ))}
-    {responseData && (
-        <div className="wrapper ai">
-            <div className="chat">
-                <div className="profile">
-                    <img src={bot} alt='bot' />
-                </div>
-                <div className="message">
-                    {Object.keys(responseData).map(key => (
-                        <Accordion key={key}>
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                                <Typography>{key}</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>{responseData[key]}</Typography>
-                            </AccordionDetails>
-                        </Accordion>
-                    ))}
-                </div>
-                <div style={{ fontSize: '0.8em', opacity: '0.6', verticalAlign: 'middle' }}>
-                    Duration: {data.duration}
-                </div>
+    useEffect(() => {
+        const lastChat = chat[chat.length - 1];
+        if (lastChat && lastChat.isAi) {
+            const uniqueId = lastChat.uniqueId;
+            const messageDiv = document.getElementById(uniqueId);
+            const loadInterval = loader(messageDiv);
+            fetch('https://codelab-agnb.onrender.com', {
+                // ... [unchanged code]
+            }).then(async response => {
+                clearInterval(loadInterval);
+                if (response.ok) {
+                    const data = await response.json();
+                    const results = data.response.result;
+                    setChat((prevChat) => prevChat.map((ch) => {
+                        if (ch.uniqueId === uniqueId) {
+                            return {
+                                ...ch,
+                                value: results,
+                                duration: data.duration,
+                            };
+                        }
+                        return ch;
+                    }));
+                } else {
+                    const err = await response.text();
+                    alert(err);
+                }
+            });
+        }
+    }, [chat, chatInputText]);
+
+    // ... [rest of the functions]
+
+    return (
+        <div id="app">
+            <div id="chat_container" ref={chatContainer}>
+                {chat.map((item, index) => (
+                    <div key={index} className={`wrapper ${item.isAi ? 'ai' : ''}`}>
+                        <div className="chat">
+                            <div className="profile">
+                                <img src={item.isAi ? bot : user} alt={item.isAi ? 'bot' : 'user'} />
+                            </div>
+                            {Array.isArray(item.value) ? (
+                                <>
+                                    <Tabs value={tabValue} onChange={(event, newValue) => setTabValue(newValue)}>
+                                        {item.value.map((_, idx) => (
+                                            <Tab label={`Result ${idx + 1}`} key={idx} />
+                                        ))}
+                                    </Tabs>
+                                    {item.value.map((result, idx) => (
+                                        <div key={idx} hidden={tabValue !== idx}>
+                                            <Card>
+                                                <CardHeader title={`Book: ${result.book}`} />
+                                                <CardContent>
+                                                    <Typography variant="body1">{result.context}</Typography>
+                                                    <Link href={result.hyperlink} target="_blank">{result.section_title}</Link>
+                                                </CardContent>
+                                            </Card>
+                                            <Typography variant="caption">Response Duration: {item.duration}</Typography>
+                                        </div>
+                                    ))}
+                                </>
+                            ) : (
+                                <div className="message" id={item.uniqueId || ''}>{item.value}</div>
+                            )}
+                        </div>
+                    </div>
+                ))}
             </div>
+            <h1>RiskExplorer Chat</h1>
+            {/* ... [rest of the JSX] */}
         </div>
-    )}
-</div>
+    );
+}
+
+export default RiskChatExplorer;
