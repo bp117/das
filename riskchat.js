d458b2d337a84a8c2bb0e132e86f21a0a66b660a
bp117 <bindupriya117@gmail.com>
2023-08-11 21:52:39 +0530

    Update riskchat.js

--- riskchat.js
+++ riskchat.js
@@ -79,3 +79,71 @@ if (response.ok) {
 }
 
 // ...
+
+
+
+import React, { useState, useRef, useEffect } from 'react';
+import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// ... (other imports)
+
+function RiskChatExplorer({chatInputText,setChatInputText}) {
+    const [chat, setChat] = useState([]);
+    // Additional state to store response data
+    const [responseData, setResponseData] = useState(null);
+    // ... (other existing code)
+
+    // ... (inside the fetch function within useEffect)
+    if (response.ok) {
+        const data = await response.json();
+        setResponseData(data.response.result); // Set the response data to state
+        const parsedData = data.bot.trim();
+        setChat(prevChat => {
+            const updatedChat = [...prevChat];
+            updatedChat[updatedChat.length - 1].value = parsedData;
+            return updatedChat;
+        });
+    } else {
+        // ... (error handling code)
+    }
+
+    // ...
+
+    return (
+        <div id="app">
+            {/* ... (existing JSX) */}
+            {responseData && (
+                <div className="wrapper ai">
+                    <div className="chat">
+                        <div className="profile">
+                            <img src={bot} alt="bot" />
+                        </div>
+                        <div className="message">
+                            <Accordion>
+                                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Typography>Response Details</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>Context: {responseData.context}</Typography>
+                                </AccordionDetails>
+                                <AccordionDetails>
+                                    <Typography>Book Hyperlink: <a href={responseData.hyperlink} target="_blank" rel="noopener noreferrer">{responseData.book}</a></Typography>
+                                </AccordionDetails>
+                                <AccordionDetails>
+                                    <Typography>Hyperlink: <a href={responseData.hyperlink} target="_blank" rel="noopener noreferrer">{responseData.hyperlink}</a></Typography>
+                                </AccordionDetails>
+                            </Accordion>
+                            <div style={{ opacity: 0.5, fontSize: "12px", textAlign: "center", marginTop: "10px" }}>
+                                Response Duration: {responseData.duration}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {/* ... (rest of the JSX) */}
+        </div>
+    );
+}
+
+export default RiskChatExplorer;
+
